b9978cdd50ac85c31621727a4e5b781804f5957f
sakaime <sakaime2@gmail.com>
2020-10-24 21:16:38 +0000

    Fix some file paths

--- docker/terraform/init.sh
+++ docker/terraform/init.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-source ~/.bashrc
+source /root/.bashrc
 
 if [[ ! -e /root/.aws/config ]]; then
-  mv ~/.aws/config.default ~/.aws/config
+  mv /root/.aws/config.default /root/.aws/config
 fi
 
 if [[ ! -e /root/.aws/credentials ]]; then
-  mv ~/.aws/credentials.default /root/.aws/credentials
+  mv /root/.aws/credentials.default /root/.aws/credentials
   sed -i "s@<access-key>@${AWS_ACCESS_KEY_ID}@g" /root/.aws/credentials
   sed -i "s@<access-key>@${AWS_SECRET_ACCESS_KEY}@g" /root/.aws/credentials
 fi
