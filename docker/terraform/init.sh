61757aa71b9dc544032521199660ded2c9dc6444
sakaime <sakaime2@gmail.com>
2020-10-25 02:27:49 +0000

    Replace delimiters in sed command

--- docker/terraform/init.sh
+++ docker/terraform/init.sh
@@ -8,6 +8,6 @@ fi
 
 if [[ ! -e /root/.aws/credentials ]]; then
   mv /root/.aws/credentials.default /root/.aws/credentials
-  sed -i "s@<access-key>@${AWS_ACCESS_KEY_ID}@g" /root/.aws/credentials
-  sed -i "s@<access-key>@${AWS_SECRET_ACCESS_KEY}@g" /root/.aws/credentials
+  sed -i "s <access-key> ${AWS_ACCESS_KEY_ID} g" /root/.aws/credentials
+  sed -i "s <access-key> ${AWS_SECRET_ACCESS_KEY} g" /root/.aws/credentials
 fi
